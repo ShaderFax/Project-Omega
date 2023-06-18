70022f532b50d02b009a5c26ad1799da5b7a073f
ShaderFax <westleyhowe@hotmail.com>
2023-06-17 20:49:46 -0600

    video pt3 finished.

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import numpy as np 
+# use "py -m ensurepip --default pip" to install pip
+# use "py -m pip install numpy" to install numpy (so happy I figured this out with google or help lol)
+
 inputs = [1, 2, 3, 2.5]
 
 weights = [[0.2, 0.8, -0.5, 1],
@@ -6,12 +10,5 @@ weights = [[0.2, 0.8, -0.5, 1],
 
 biases = [2, 3, 0.5]
 
-layer_outputs = []
-for neuron_weights, neuron_bias, in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-
-print(layer_outputs)
+output = np.dot(weights, inputs) + biases
+print(output)
