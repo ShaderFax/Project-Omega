1f47ea73617e7805fcc037120a36e24b80e4128c
westley howe <westman669@gmail.com>
2023-06-30 02:07:10 -0600

    part 5 done

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-import numpy as np 
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
 # use "py -m ensurepip --default pip" to install pip
 # use "py -m pip install numpy" to install numpy (so happy I figured this out with google or help lol)
+# use "py - m pip install nnfs" to install nnfs package
 
 np.random.seed(0)
 
 X = [[1, 2, 3, 2.5],
     [2.0, 5.0, -1.0, 2.0],
     [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
         def __init__(self, n_inputs, n_neurons):
@@ -15,11 +23,17 @@ class Layer_Dense:
         def forward(self, inputs):
                 self.output = np.dot(inputs, self.weights) + self.biases
 
+class Activation_ReLU:
+        def forward(self, inputs):
+                self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(4, 6)
-layer2 = Layer_Dense(6, 3)
+
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
+
 #print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)
