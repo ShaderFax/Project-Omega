eb0693846372ae568f45ccd91719aa83f7883fa7
ShaderFax <westleyhowe@hotmail.com>
2023-07-13 02:51:46 -0600

    part 6 finished

--- app.py
+++ app.py
@@ -6,15 +6,7 @@ nnfs.init()
 
 # use "py -m ensurepip --default pip" to install pip
 # use "py -m pip install numpy" to install numpy (so happy I figured this out with google or help lol)
-# use "py - m pip install nnfs" to install nnfs package
-
-np.random.seed(0)
-
-X = [[1, 2, 3, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
+# use "py -m pip install nnfs" to install nnfs package
 
 class Layer_Dense:
         def __init__(self, n_inputs, n_neurons):
@@ -27,13 +19,24 @@ class Activation_ReLU:
         def forward(self, inputs):
                 self.output = np.maximum(0, inputs)
 
+class Activation_Softmax:
+        def forward(self, inputs):
+                exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+                probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+                self.output = probabilities
 
+X,y = spiral_data(samples=100, classes=3)
 
-layer1 = Layer_Dense(2, 5)
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
 
-#print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
