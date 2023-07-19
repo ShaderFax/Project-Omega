d657e6c89a83e210f5fcfc0dd10030acb2deda59
ShaderFax <westleyhowe@hotmail.com>
2023-07-19 01:58:01 -0600

    starting work, installed tensorflow and transformers datasets

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ nnfs.init()
 # use "py -m ensurepip --default pip" to install pip
 # use "py -m pip install numpy" to install numpy (so happy I figured this out with google or help lol)
 # use "py -m pip install nnfs" to install nnfs package
+# use "py -m pip install transformers datasets" to install datasets
+# use "py -m pip install tensorflow" to install tensorflow
 
 class Layer_Dense:
         def __init__(self, n_inputs, n_neurons):
