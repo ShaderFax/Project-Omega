7f0e2bae6e49c681a344b1d0250a015d4a5834d7
ShaderFax <westleyhowe@hotmail.com>
2023-10-10 00:55:06 -0600

    Working on tts

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
 import numpy as np
 
-# use "py -m ensurepip --default pip" to install pip
-# use "py -m pip install numpy" to install numpy (so happy I figured this out with google or help lol)
-# use "py -m pip install nnfs" to install nnfs package
-# use "py -m pip install transformers datasets" to install datasets
-# use "py -m pip install tensorflow" to install tensorflow
-
