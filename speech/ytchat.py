c5bcab68caf24f85e30a421b94fd1fb1f340a16c
ShaderFax <westleyhowe@hotmail.com>
2023-10-09 23:19:55 -0600

    Finished youtube script

--- speech/ytchat.py
+++ speech/ytchat.py
@@ -3,3 +3,6 @@ import pytchat as ytc
 # install pytchat w/ "py -m pip install pytchat"
 
 chat = ytc.create(video_id="XxVZYEffgpQ")
+while chat.is_alive():
+    for c in chat.get().sync_items():
+        print(f"{c.message}")
