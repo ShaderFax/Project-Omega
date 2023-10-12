c303c4db2b14d68a6fa087e75637df023d5a8f2a
ShaderFax <westleyhowe@hotmail.com>
2023-10-12 01:27:12 -0600

    just pushing.

--- speech/ytchat.py
+++ speech/ytchat.py
@@ -2,7 +2,7 @@ import pytchat as ytc
 
 # install pytchat w/ "py -m pip install pytchat"
 
-chat = ytc.create(video_id="XxVZYEffgpQ")
+chat = ytc.create(video_id="")
 while chat.is_alive():
     for c in chat.get().sync_items():
         print(f"{c.message}")
