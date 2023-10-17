4262dda1b4d2c14ee0f4a7b779b8556cc2673f5c
ShaderFax <westleyhowe@hotmail.com>
2023-10-16 20:04:31 -0600

    Changing from LLaMa 2 to gpt 3.5

--- Chat_Bot/bot.py
+++ Chat_Bot/bot.py
@@ -1,53 +1,6 @@
-from transformers import pipeline
-from transformers import AutoTokenizer, AutoModelForCausalLM, logging
-import torch
+import openai as oai
+from speech import ytchat
 
-model = "TheBloke/Nous-Hermes-Llama-2-7B-GPTQ"
+oai.api_key = "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"
 
-# configure the generator
-gen_model = AutoModelForCausalLM.from_pretrained(
-    model,
-    torch_dtype=torch.bfloat16,
-    device_map="auto",
-    revision="main"
-)
-
-gen_tokenizer = AutoTokenizer.from_pretrained(model, use_fast=True)
-
-pipe = pipeline(
-    "text-generation",
-    model=gen_model,
-    tokenizer=gen_tokenizer,
-    max_new_tokens=128,
-    repetition_penalty=1.15
-)
-
-# configure the POS
-logging.set_verbosity(logging.CRITICAL)
-
-# store chat history
-chat_history = []
-
-# prompt template string
-prompt_template = """Below is an instruction that describes a task. Write a response that appropriately completes the request. 
-
-### Instruction: You are an ai chatbot named Omega-chan. You are in a conversation between you and a friendly human. Continue the conversation.
-
-{history}
-
-### Response:
-Omega-chan:
-"""
-
-while True:
-    # read input & store it
-    user_message = input("human: ")
-    chat_history.append("human: " + user_message)
-
-    # generate output
-    prompt = prompt_template.format(history="\n".join(chat_history))
-    out = "Omega-chan: " + pipe(prompt)[0]["generated_text"]
-
-    # store output & print it
-    chat_history.append(out)
-    print(out)
+messages = [ytchat.message]
--- speech/ytchat.py
+++ speech/ytchat.py
@@ -1,4 +1,5 @@
 import pytchat as ytc
+import time
 
 # install pytchat w/ "py -m pip install pytchat"
 
@@ -6,3 +7,4 @@ chat = ytc.create(video_id="")
 while chat.is_alive():
     for c in chat.get().sync_items():
         print(f"{c.message}")
+        time.sleep(30)
