2ccbd0b9befbecf37e46e366f21f86a161ff7e11
ShaderFax <westleyhowe@hotmail.com>
2023-10-22 03:23:33 -0600

    omega-chan won't respond to youtube chat message.

--- speech/ytchat.py
+++ speech/ytchat.py
@@ -1,8 +1,6 @@
 import pytchat as ytc
 
-# install pytchat w/ "py -m pip install pytchat"
-
 chat = ytc.create(video_id="XxVZYEffgpQ")
 while chat.is_alive():
     for c in chat.get().sync_items():
-        print(f"{c.message}")
+        print(f"[{c.author.name}]{c.message}\n")
