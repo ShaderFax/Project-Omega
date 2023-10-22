1c0b01c9b4e1504f9535296cb44877ca44efe24a
ShaderFax <westleyhowe@hotmail.com>
2023-10-21 23:07:25 -0600

    fixed bug, begin wiring, then test stream it.

--- speech/bot.py
+++ speech/bot.py
@@ -1,21 +1,24 @@
 import os
 import openai as oai
 import sys
-from ytchat import chatMsg
+# from ytchat import message as msg
 
 oai.api_key = "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"
 
-messages = []
-system_msg = input("Describe the character you want to me to play\n")
+messages = [{
+      "role": "system",
+      "content": "You are an ai vtuber called omega-chan who is enthusiastic and loves games."
+    }]
+system_msg = ""
 messages.append({"role": "system", "content": system_msg})
 
-def chatBot():
-    while input != "quit()":
-        message = input()
-        messages.append({"role": "system", "content": message})
-        response = oai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages)
-        reply = response["choices"][0]["message"]["content"]
-        messages.append({"role": "system", "content": reply})
-        print("\n"+reply+"\n")
+
+while input != "quit()":
+    message = input()
+    messages.append({"role": "system", "content": message})
+    response = oai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages)
+    reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "system", "content": reply})
+    print("\n"+reply+"\n")
--- speech/ytchat.py
+++ speech/ytchat.py
@@ -1,10 +1,8 @@
 import pytchat as ytc
-import time
 
 # install pytchat w/ "py -m pip install pytchat"
 
-def chatMsg():
-    chat = ytc.create(video_id="XxVZYEffgpQ")
-    while chat.is_alive():
-        for c in chat.get().sync_items():
-            print(f"[{c.author.name}]{c.message}")
+chat = ytc.create(video_id="XxVZYEffgpQ")
+while chat.is_alive():
+    for c in chat.get().sync_items():
+        print(f"[{c.author.name}] {c.message}")
