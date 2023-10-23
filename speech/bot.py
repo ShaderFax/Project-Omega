6c21006a89cce9e1858682882386894d708337a2
ShaderFax <westleyhowe@hotmail.com>
2023-10-23 03:13:35 -0600

    still bugged

--- speech/bot.py
+++ speech/bot.py
@@ -1,19 +1,20 @@
-import os
 import openai as oai
-import sys
+from pynput.keyboard import Key, Controller
 from ytchat import chat
 
 oai.api_key = "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"
 
-messages = [{
-      "role": "system",
-      "content": "You are an ai vtuber called omega-chan, you are enthusiastic."
-    }]
-system_msg = ""
+keyboard = Controller()
+
+messages = []
+system_msg = input("Info to input: \n")
 messages.append({"role": "system", "content": system_msg})
+keyboard.tap(Key.enter)
+print("info was entered")
 
 while input != "quit()":
-    msg = input()
+    msg = input(chat)
+    keyboard.tap(Key.enter)
     messages.append({"role": "system", "content": msg})
     response = oai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -21,3 +22,4 @@ while input != "quit()":
     reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "system", "content": reply})
     print("\n"+reply+"\n")
+    print("reply has been made")
--- speech/ytchat.py
+++ speech/ytchat.py
@@ -1,6 +1,10 @@
 import pytchat as ytc
+from pynput.keyboard import Key, Controller
+
+KB = Controller()
 
 chat = ytc.create(video_id="XxVZYEffgpQ")
 while chat.is_alive():
     for c in chat.get().sync_items():
         print(f"[{c.author.name}]{c.message}\n")
+        KB.tap(Key.enter)
