4992884333fdb5a4b2ee49dbe49cb9267bd89323
ShaderFax <westleyhowe@hotmail.com>
2023-10-23 17:03:46 -0600

    fixed issue, working, needs a chat cooldown timer

--- speech/bot.py
+++ speech/bot.py
@@ -1,25 +1,25 @@
 import openai as oai
 from pynput.keyboard import Key, Controller
-from ytchat import chat
+import pytchat as ytc
+import time
 
 oai.api_key = "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"
-
-keyboard = Controller()
 
 messages = []
 system_msg = input("Info to input: \n")
 messages.append({"role": "system", "content": system_msg})
-keyboard.tap(Key.enter)
-print("info was entered")
+print("info was entered\n")
 
-while input != "quit()":
-    msg = input(chat)
-    keyboard.tap(Key.enter)
-    messages.append({"role": "system", "content": msg})
-    response = oai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages)
-    reply = response["choices"][0]["message"]["content"]
-    messages.append({"role": "system", "content": reply})
-    print("\n"+reply+"\n")
-    print("reply has been made")
+chat = ytc.create(video_id="XxVZYEffgpQ")
+while chat.is_alive():
+    for c in chat.get().sync_items():
+        print(f"[{c.author.name}]{c.message}\n")
+        msg = c.message
+        messages.append({"role": "user", "content": msg})
+        response = oai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages)
+        reply = response["choices"][0]["message"]["content"]
+        messages.append({"role": "assistant", "content": reply})
+        print("\n"+reply+"\n")
+        print("reply has been made\n")
--- speech/ytchat.py
+++ speech/ytchat.py
@@ -1,10 +1,8 @@
 import pytchat as ytc
 from pynput.keyboard import Key, Controller
-
-KB = Controller()
+import asyncio
 
 chat = ytc.create(video_id="XxVZYEffgpQ")
 while chat.is_alive():
     for c in chat.get().sync_items():
         print(f"[{c.author.name}]{c.message}\n")
-        KB.tap(Key.enter)
